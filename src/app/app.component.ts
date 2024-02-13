2d7c5990b59f8778ad758e27dd2bdcffc3b9cc8b
unknown <kumarujjawal1408@gmail.com>
2024-02-13 14:47:48 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,101 @@ import { ApiService } from './services/api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
+  searchQuery: string = '';
+  userImage: string = ''; 
+  bio: string='';
+  name:string='';
+  company:string='';
+  location:string='';
+  followers:number=0;
+  following:number=0;
+  repos: number = 0;
+  url: string='';
+  publicRepos: any[] = [];
+  topics: string[] = [];
+  pageSize: number = 10;
+  currentPage: number = 1;
+  paginatedRepos: any[] = [];
+  totalPages: number = 0;
+  searchExecuted: boolean = false;
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.apiService.getRepos('johnpapa').subscribe(console.log);
   }
+
+  search() {
+    this.searchExecuted=true;
+    this.apiService.getUser(this.searchQuery).subscribe((data: any) => {
+      if (data.name) {
+        this.userImage = data.avatar_url;
+        this.name = data.name;
+        this.bio = data.bio;
+        this.company = data.company;
+        this.location = data.location;
+        this.followers = data.followers;
+        this.following = data.following;
+        this.repos = data.public_repos;
+        this.url = data.url;
+  
+        this.getPaginatedRepos(); // Fetch paginated repositories
+      } else {
+        this.resetUserData();
+      }
+    });
+  }
+
+  resetUserData() {
+    this.userImage = '';
+    this.name = '';
+    this.bio = '';
+    this.company = '';
+    this.location = '';
+    this.followers = 0;
+    this.following = 0;
+    this.repos = 0;
+    this.url = '';
+    this.publicRepos = [];
+    this.paginatedRepos = [];
+  }
+  getPaginatedRepos() {
+    this.apiService.getPaginatedRepos(this.searchQuery, this.pageSize,this.currentPage)
+      .subscribe((data: any) => {
+        this.paginatedRepos = data;
+        this.calculateTotalPages();
+      });
+  }
+  
+calculateTotalPages() {
+  this.totalPages = Math.ceil(this.repos / this.pageSize);
+}
+  changePageSize() {
+    this.currentPage = 1;
+    this.getPaginatedRepos();
+  }
+
+  onPageChange(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+  
+  nextPage() {
+    const nextPageNumber = this.currentPage + 1;
+    const totalPages = Math.ceil(this.repos / this.pageSize);
+  
+    if (nextPageNumber <= totalPages) {
+      this.currentPage = nextPageNumber;
+      this.getPaginatedRepos();
+    }
+  }
+  
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.getPaginatedRepos();
+    }
+  }
+  
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +14,19 @@ export class ApiService {
     private httpClient: HttpClient
   ) { }
 
-  getUser(githubUsername: string) {
+  getUser(githubUsername: string) : Observable<any>{
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string): Observable<any> {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`);
+  }
+   // Get paginated repositories
+   getPaginatedRepos(githubUsername: string, pageSize: number, page: number): Observable<any> {
+    const perPage = pageSize > 100 ? 100 : pageSize; // Limit page size to maximum 100
+    const url = `https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${perPage}`;
+    return this.httpClient.get(url);
+    return this.httpClient.get(url).pipe(shareReplay());
+  } 
 }
